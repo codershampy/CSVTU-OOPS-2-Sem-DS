cd426096927376b4d8764b8fbcab0a6200303109
Shampy <157746114+codershampy@users.noreply.github.com>
2024-04-11 00:15:55 +0530

    Add files via upload

---
+++ Program to swap two numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,temp;
+    cout<<"enter the value of a:";
+    cin>>a;
+    cout<<"enter the value of b:";
+    cin>>b;
+    cout<<"after swapping the two numbers";
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<"\n a= "<<a;
+    cout<<"\n b= "<<b;
+    return 0; 
+}
