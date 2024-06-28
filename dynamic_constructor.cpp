4622b8c1fbec6806d311a515dd2ab405f02ab272
Shampy <157746114+codershampy@users.noreply.github.com>
2024-06-28 23:19:39 +0530

    add

---
+++ dynamic_constructor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class geeks {
+	const char* p;
+public:
+	// default constructor
+	geeks()
+	{
+	// allocating memory at run time
+		p = new char[6];
+		p = "geeks";
+	}
+    void display() { cout << p << endl; }
+};
+
+int main()
+{
+	geeks obj;
+	obj.display();
+}
