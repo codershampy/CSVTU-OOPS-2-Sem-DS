9ead61b318e712e8e5c006a7d8b041b9ad7ec704
Shampy <157746114+codershampy@users.noreply.github.com>
2024-08-19 16:00:08 +0530

    Add files via upload

---
+++ static_function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class Student {
+public:
+	// static member
+	static int total;
+// Constructor called
+	Student() { total += 1; }
+};
+int Student::total = 0;
+int main()
+{
+	Student s1;
+	cout << "Number of students:" << s1.total << endl;
+	Student s2;
+	cout << "Number of students:" << s2.total << endl;
+	Student s3;
+	cout << "Number of students:" << s3.total << endl;
+	return 0;
+}
